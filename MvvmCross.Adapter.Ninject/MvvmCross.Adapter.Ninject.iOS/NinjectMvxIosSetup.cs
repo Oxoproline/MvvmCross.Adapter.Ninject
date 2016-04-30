dbe6ef347ad5036ad86c959e5f30bd7a441ccbcb
Przemyslaw Raciborski <przemyslawraciborski@gmail.com>
2016-04-30 23:32:43 +0200

    Added iOS Project and implemented NinjectMvxIosSetup.

---
+++ MvvmCross.Adapter.Ninject/MvvmCross.Adapter.Ninject.iOS/NinjectMvxIosSetup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.iOS.Platform;
+using MvvmCross.iOS.Views.Presenters;
+using UIKit;
+
+namespace MvvmCross.Adapter.Ninject.iOS
+{
+    public abstract class NinjectMvxIosSetup : MvxIosSetup
+    {
+        private readonly NinjectDependenciesProvider _dependenciesProvider;
+
+        protected NinjectMvxIosSetup(IMvxApplicationDelegate applicationDelegate, UIWindow window, NinjectDependenciesProvider dependenciesProvider) : base(applicationDelegate, window)
+        {
+            _dependenciesProvider = dependenciesProvider;
+        }
+
+        protected NinjectMvxIosSetup(IMvxApplicationDelegate applicationDelegate, IMvxIosViewPresenter presenter) : base(applicationDelegate, presenter)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
