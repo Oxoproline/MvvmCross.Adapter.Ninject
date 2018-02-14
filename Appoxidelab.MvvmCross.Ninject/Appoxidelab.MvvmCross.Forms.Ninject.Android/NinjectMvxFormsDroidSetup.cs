ba39295ee956260942760a5107b57640458ce884
Unicorn <filip.mokry@pwc.com>
2018-02-14 14:32:33 +0100

    added support for mvxFormsSetup

---
+++ Appoxidelab.MvvmCross.Ninject/Appoxidelab.MvvmCross.Forms.Ninject.Android/NinjectMvxFormsDroidSetup.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Android.Content;
+using Appoxidelab.MvvmCross.Ninject.Shared;
+using MvvmCross.Forms.Droid.Platform;
+using MvvmCross.Platform.IoC;
+
+namespace Appoxidelab.MvvmCross.Forms.Ninject.Android
+{
+    public abstract class NinjectMvxFormsDroidSetup : MvxFormsAndroidSetup
+    {
+        protected NinjectMvxFormsDroidSetup(Context applicationContext) : base(applicationContext)
+        {
+        }
+
+        protected override void InitializeLastChance()
+        {
+            base.InitializeLastChance();
+
+            (NinjectMvxIoCProvider.Instance as NinjectMvxIoCProvider).ExecuteDelayedCallback();
+        }
+
+        protected abstract NinjectDependenciesProvider GetNinjectDependenciesProvider();
+
+        protected sealed override IMvxIoCProvider CreateIocProvider()
+            => new NinjectMvxIoCProvider(GetNinjectDependenciesProvider().GetNinjectModules().ToArray());
+    }
+}
---
+++ Appoxidelab.MvvmCross.Ninject/Appoxidelab.MvvmCross.Forms.Ninject.iOS/NinjectMvxFormsIosSetup.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Appoxidelab.MvvmCross.Ninject.Shared;
+using MvvmCross.Forms.iOS;
+using MvvmCross.iOS.Platform;
+using MvvmCross.iOS.Views.Presenters;
+using MvvmCross.Platform.IoC;
+using UIKit;
+
+namespace Appoxidelab.MvvmCross.Forms.Ninject.iOS
+{
+    public abstract class NinjectMvxFormsIosSetup : MvxFormsIosSetup
+    {
+        protected NinjectMvxFormsIosSetup(IMvxApplicationDelegate applicationDelegate, UIWindow window)
+            : base(applicationDelegate, window)
+        {
+        }
+
+        protected NinjectMvxFormsIosSetup(IMvxApplicationDelegate applicationDelegate, IMvxIosViewPresenter presenter)
+            : base(applicationDelegate, presenter)
+        {
+        }
+
+        protected abstract NinjectDependenciesProvider GetNinjectDependenciesProvider();
+
+        protected override void InitializeLastChance()
+        {
+            base.InitializeLastChance();
+
+            (NinjectMvxIoCProvider.Instance as NinjectMvxIoCProvider).ExecuteDelayedCallback();
+        }
+
+        protected sealed override IMvxIoCProvider CreateIocProvider()
+            => new NinjectMvxIoCProvider(GetNinjectDependenciesProvider().GetNinjectModules().ToArray());
+    }
+}
